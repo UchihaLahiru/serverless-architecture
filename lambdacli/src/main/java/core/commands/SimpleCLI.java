7056252bb14c611c69a9c4fe7d9948a66bd0cd67
maanadev <maanafunedu@gmail.com>
2017-08-16 16:38:15 +0530

    commmand skeloton

--- lambdacli/src/main/java/core/commands/SimpleCLI.java
+++ lambdacli/src/main/java/core/commands/SimpleCLI.java
@@ -11,20 +11,11 @@ import java.io.PrintWriter;
 @Component
 public class SimpleCLI implements CommandMarker {
 
-    @CliCommand(value = { "web-get", "wg" })
-    public String webGet(
-            @CliOption(key = "url") String url) {
+    @CliCommand(value = { "create-func", "cf" })
+    public String createFuntion(
+            @CliOption(key = "name") String name,
+            @CliOption(key = "file") String file) {
         return "ok";
     }
 
-    @CliCommand(value = { "web-save", "ws" })
-    public String webSave(
-            @CliOption(key = "url") String url,
-            @CliOption(key = { "out", "file" }) String file) throws FileNotFoundException {
-        String contents = url;
-        try (PrintWriter out = new PrintWriter(file)) {
-            out.write(contents);
-        }
-        return "Done.";
-    }
 }
